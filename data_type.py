3b537475062be74abc3b090b31c1de937887e886
anchalgupta2964-wq <anchal.gupta2964@gmail.com>
2026-07-23 21:09:08 +0530

    Day - 1

---
+++ data_type.py
@@ -0,0 +1,10 @@
+a=32
+b=32.4
+c = "Anchal"
+d = True
+e = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ identifires.py
@@ -0,0 +1,7 @@
+a = 23
+b =45.4
+c=b # c and b have the same memory address
+print(id(a))# it s used to find address of the variable
+print(id(b))
+print(id(c))
+
